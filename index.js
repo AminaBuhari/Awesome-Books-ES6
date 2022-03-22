e6220f5a62a0d816f0fca63a0436e953493d5986
buhariaminat <buhariaminat@yahoo.com>
2022-03-22 09:27:19 +0100

    Add date using luxon file package to main js

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ add();
 // Add Nav Display
 import {navDisplay} from './module/navigation.js'
 navDisplay;
+// Add date
+import {DateTime} from './node_modules/luxon/src/luxon.js';
+const date = window.document.getElementById('date');
+const nowdate= () => {
+    let year = DateTime.now().year;
+    let month = DateTime.now().month;
+    let day = DateTime.now().day;
+    let hour = DateTime.now().hour;
+    let minute = DateTime.now().minute;
+    let second = DateTime.now().second;
+    date.innerHTML = `${year}/${month}/${day} ${hour}:${minute}:${second}`;
+}
+setInterval(nowdate, 1000);
