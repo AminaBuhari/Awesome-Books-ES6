f720714121dc5e9c356def420ea1373a82e81d4b
buhariaminat <buhariaminat@yahoo.com>
2022-03-22 14:46:32 +0100

    Catched node modules with git ignore file

--- module/navigation.js
+++ module/navigation.js
@@ -1,16 +1,18 @@
 import { navItems, sectionBooks } from './DOM.js';
 
-const navDisplay = navItems.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    for (let i = 0; i < navItems.length; i += 1) {
-      sectionBooks[i].style.display = 'none';
-    }
-    sectionBooks[index].style.display = 'flex';
+const navDisplay = () => {
+  navItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      for (let i = 0; i < navItems.length; i += 1) {
+        sectionBooks[i].style.display = 'none';
+      }
+      sectionBooks[index].style.display = 'flex';
+    });
   });
-});
 
-navItems[0].addEventListener('click', () => {
-  window.location.reload();
-});
+  navItems[0].addEventListener('click', () => {
+    window.location.reload();
+  });
+};
 
 export default navDisplay;
